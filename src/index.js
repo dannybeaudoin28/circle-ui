f391f13c4b22a253a9ea267e36315df32d963ec9
dannybeaudoin28 <danny.beaudoin@student.sl.on.ca>
2024-04-05 05:01:17 -0400

    Add react-query, connect to api for users admin panel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/routes/admin-panel/admin-panel.component.jsx
+++ src/routes/admin-panel/admin-panel.component.jsx
@@ -1,16 +1,46 @@
 import './admin-panel.styles.css';
+import { useQuery } from '@tanstack/react-query';
 
 const AdminPanel = () => {
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['userData'],
+        queryFn: () =>
+            fetch('http://localhost:8888/users/get-users').then((res) =>
+                res.json(),
+            ),
+    });
+
+    if (isLoading) return 'Loading...';
+
+    if (error) return 'An error has occurred: ' + error.message;
+
     return (
         <div className='testing'>
-            <p>testing</p>
-            <p>testing</p>
-            <p>testing</p>
-            <p>testing</p>
-            <p>testing</p>
-            <p>testing</p>
+            <h1>Users:</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Middle Initial</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((userData) => (
+                        <tr key={userData.userId}>
+                            <td>{userData.userId}</td>
+                            <td>{userData.userEmail}</td>
+                            <td>{userData.userFName}</td>
+                            <td>{userData.userMiddleInitial}</td>
+                            <td>{userData.userLName}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-      );
-}
+    );
+};
 
 export default AdminPanel;
